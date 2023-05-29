33495469d7159957066a9d02e2ffef89a6003c45
Ana Barbara Campos Conti <anabarbara.conti@gmail.com>
2023-05-29 20:03:34 -0300

    feat: criacao da classe Cliente

---
+++ IstituicaoFinanceira/src/Cliente.java
@@ -0,0 +1,63 @@
+public class Cliente extends Pessoa{
+    private String escolaridade;
+    private Agencia agencia;
+
+    public Cliente(){
+        super();
+        this.escolaridade="";
+        agencia = new Agencia();
+    }
+
+    public Cliente(String cpf, String nome){
+        super(cpf, nome);
+    }
+
+    @Override
+    public String getNome() {
+        return super.getNome();
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    @Override
+    public String getCpf() {
+        return super.getCpf();
+    }
+
+    @Override
+    public void setCpf(String cpf) {
+        super.setCpf(cpf);
+    }
+
+    public String getEscolaridade() {
+        return escolaridade;
+    }
+
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "escolaridade='" + escolaridade + '\'' +
+                ", agencia=" + agencia +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", estadoCivil='" + estadoCivil + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                '}';
+    }
+}
