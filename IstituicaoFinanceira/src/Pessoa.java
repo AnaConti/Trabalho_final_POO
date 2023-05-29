2162d391484793fe9a6f3781543be3e901c9e0ec
Ana Barbara Campos Conti <anabarbara.conti@gmail.com>
2023-05-29 19:58:26 -0300

    feat: criacao da classe Pessoa

---
+++ IstituicaoFinanceira/src/Pessoa.java
@@ -0,0 +1,67 @@
+public abstract class Pessoa {
+    protected String cpf, nome, estadoCivil, endereco, dataNascimento;
+
+    public Pessoa(){
+        this.cpf = " ";
+        this.nome = " ";
+        this.endereco = " ";
+        this.estadoCivil= " ";
+        this.dataNascimento = " ";
+    }
+
+    public Pessoa(String cpf, String nome){
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", estadoCivil='" + estadoCivil + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                '}';
+    }
+}
