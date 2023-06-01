9da8df8af32f502cbf92d91e8e7d0e1ed4487fbc
Ana Barbara Campos Conti <anabarbara.conti@gmail.com>
2023-06-01 08:30:34 -0300

    feat: criacao da classe Agencia

---
+++ IstituicaoFinanceira/src/Agencia.java
@@ -0,0 +1,44 @@
+public class Agencia {
+    private int numero;
+    private String nome, endereco;
+    private Gerente gerente;
+
+    public Agencia(){
+        this.numero=000;
+        this.nome="";
+        this.endereco='';
+        this.gerente= new Gerente();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Gerente getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+}
