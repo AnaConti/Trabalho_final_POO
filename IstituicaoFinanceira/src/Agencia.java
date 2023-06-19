48f9de6095061c184fd2636b9fe59e8bbb64c567
Ana Barbara Campos Conti <anabarbara.conti@gmail.com>
2023-06-19 20:26:00 -0300

    feat: add de funcionarios/contas na classe Agencia

--- IstituicaoFinanceira/src/Agencia.java
+++ IstituicaoFinanceira/src/Agencia.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+
 public class Agencia {
     private int numero;
     private String nome, endereco;
+
+    private ArrayList<Conta> contas;
+    private ArrayList<Funcionario> funcionarios;
     private Gerente gerente;
 
     public Agencia(){
         this.numero=000;
         this.nome="";
         this.endereco="";
+        this.contas= new ArrayList<>();
+        this.funcionarios= new ArrayList<>();
         this.gerente= new Gerente();
+    }
+
+     public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
     }
 
     public int getNumero() {
@@ -40,5 +55,10 @@ public class Agencia {
 
     public void setGerente(Gerente gerente) {
         this.gerente = gerente;
+    }
+
+     public String toString() {
+        return "AgenciaBancaria{" + "numero=" + numero + "\n nome='" + nome + '\'' + "\n endereco" 
+         + "\n contas=" + contas + "\n funcionarios=" + funcionarios + '}';
     }
 }
