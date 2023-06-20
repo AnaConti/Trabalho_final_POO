2b41476bfe111c09fa9373fdd86f4f69278e9a6b
Ana Barbara Campos Conti <anabarbara.conti@gmail.com>
2023-06-19 21:49:41 -0300

    feat: metodo na classe efetuar pagamento

--- IstituicaoFinanceira/src/EfetuarPagamento.java
+++ IstituicaoFinanceira/src/EfetuarPagamento.java
@@ -14,9 +14,26 @@ public class EfetuarPagamento extends Transacao{
         this.valor = valor;
     }
 
+    public void efetuarPagamento(double valor){
+        if(solicitarSenha()){
+            if(valor>0 && valor>super.conta.getSaldoAtual()){
+                double saldo=super.conta.getSaldoAtual();
+                saldo-=valor;
+            
+                super.conta.setSaldoAtual(saldo);
+                super.conta.updateUltimaMovimentacao();
+            }else{
+                System.out.println("Saldo insuficiente");
+            }
+        }else{
+            System.out.println("Senha incorreta");
+        }
+        
+    }
+
     @Override
     public String toString() {
-        return "EfetuarPagamento{" +
+        return  super.toString() + "EfetuarPagamento{" +
                 "valor=" + valor +
                 '}';
     }
