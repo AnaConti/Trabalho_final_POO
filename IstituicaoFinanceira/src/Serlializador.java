a7e175e909c97a5419942d45dd3e50572ab63c68
Gabriel Couto de Freitas <gabrielcouto3.14@gmail.com>
2023-06-26 11:27:32 -0300

    feat: classe serializador pronta, serializa a o objeto Main

---
+++ IstituicaoFinanceira/src/Serlializador.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.concurrent.ExecutionException;
+
+public class Serlializador implements Serializable {
+    public void serializar(String arq, Main instituicaoFinanceira) throws IllegalArgumentException{
+
+        if(arq == null || arq.isEmpty())
+            throw new IllegalArgumentException("Nome de arqivo inválido!");
+        else if(instituicaoFinanceira==null)
+            throw new IllegalArgumentException("Insituição financeira vazia");
+
+        try {
+            FileOutputStream file    = new FileOutputStream(arq);
+            ObjectOutputStream banco = new ObjectOutputStream(file);
+
+            banco.writeObject(instituicaoFinanceira);
+
+            banco.close();
+            file.close();
+
+            System.out.println("Banco salvo!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Main desserializar(String arq, Main instituicaoFinanceira){
+        if(arq==null||arq.isEmpty())
+            throw new IllegalArgumentException("Nome de arquivo inválido!");
+
+        try {
+            FileInputStream file = new FileInputStream(arq);
+            ObjectInputStream banco = new ObjectInputStream(file);
+            Main obj = (Main) banco.readObject();
+            banco.close();
+            file.close();
+            System.out.println("Banco carregado!");
+            return obj;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
